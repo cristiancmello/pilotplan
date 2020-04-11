0b2c5424b99f96a26f3dbf0884d45d97706c0ae2
Cristian Mello <cristianc.mello@gmail.com>
2020-04-11 20:05:35 -0300

    Enable cdk destroy test

--- tests/cdk-builder/handler.test.js
+++ tests/cdk-builder/handler.test.js
@@ -2,9 +2,11 @@ const AWS = require("aws-sdk");
 
 const { CdkDeployParams } = require("../../src/common/CdkDeployParams");
 const { callCdkDeploySync } = require("../../src/common/cdkDeployCommand");
+const { CdkDestroyParams } = require("../../src/common/CdkDestroyParams");
+const { callCdkDestroySync } = require("../../src/common/cdkDestroyCommand");
 
 beforeEach(async () => {
-  jest.setTimeout(15000);
+  jest.setTimeout(90000);
 });
 
 test("call cdkDeploy without errors", async () => {
@@ -30,5 +32,31 @@ test("call cdkDeploy without errors", async () => {
     )
   );
 
-  console.log(response);
+  expect(response.error).toBeUndefined();
+});
+
+test("call cdkDestroy without errors", async () => {
+  let awsAccessKeyId = null,
+    awsSecretAccessKey = null;
+
+  if (process.env.NODE_ENV === "test") {
+    awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+  }
+
+  const response = await callCdkDestroySync(
+    new CdkDeployParams(
+      "emptyApp",
+      "us-east-1",
+      {
+        test: {
+          message: "hello, world!",
+        },
+      },
+      awsAccessKeyId,
+      awsSecretAccessKey
+    )
+  );
+
+  expect(response.error).toBeUndefined();
 });
