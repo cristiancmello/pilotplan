30f1a43f7730b4955577e817e6d9fbf4d403bfe2
Cristian Mello <cristianc.mello@gmail.com>
2020-04-16 14:27:20 -0300

    Add setupRexrayPlugin in ManagerEntrypointFile

--- src/cdkBuilder/components/SwarmBasicCluster/manager/EntrypointFile.js
+++ src/cdkBuilder/components/SwarmBasicCluster/manager/EntrypointFile.js
@@ -15,23 +15,90 @@ const ManagerEntrypointFile = (stackName, credentials, setup = {}) => {
     installPortainerScriptString = installPortainerScript();
   }
 
+  const rexrayCredentials = {
+    ebs: {
+      aws_region: credentials.aws_region,
+      aws_access_key_id: credentials.aws_access_key_id,
+      aws_secret_access_key: credentials.aws_secret_access_key,
+    },
+    s3fs: {
+      aws_access_key_id: credentials.aws_access_key_id,
+      aws_secret_access_key: credentials.aws_secret_access_key,
+    },
+    efs: {
+      aws_access_key_id: credentials.aws_access_key_id,
+      aws_secret_access_key: credentials.aws_secret_access_key,
+    },
+  };
+
   return `
 ${swarmNodeManagerEntrypointFileString}
+${setupRexrayPlugin(rexrayCredentials)}
 ${installPortainerScriptString}
 ${setupNodeManagerCreationControl(stackName, credentials)}
+`;
+};
+
+const setupRexrayPlugin = (rexrayCredentials) => {
+  return `
+# {id:setupRexrayPlugin}
+# Setup Rexray Plugin
+export EBS_REGION=${rexrayCredentials.ebs.aws_region}
+export EBS_ACCESSKEY=${rexrayCredentials.ebs.aws_access_key_id}
+export EBS_SECRETKEY=${rexrayCredentials.ebs.aws_secret_access_key}
+
+export S3FS_ACCESSKEY=${rexrayCredentials.s3fs.aws_access_key_id}
+export S3FS_SECRETKEY=${rexrayCredentials.s3fs.aws_secret_access_key}
+
+export EFS_ACCESSKEY=${rexrayCredentials.efs.aws_access_key_id}
+export EFS_SECRETKEY=${rexrayCredentials.efs.aws_secret_access_key}
+
+sudo sh -c "cat << EOF >> /etc/rexray/config.yml
+libstorage:
+  service: ebs
+  server:
+    services:
+      ebs:
+        driver: ebs
+      s3fs:
+        driver: s3fs
+      efs:
+        driver: efs
+EOF"
+
+sudo systemctl start rexray
+sleep 3
+
+docker plugin install rexray/ebs \
+    REXRAY_PREEMPT=true \
+    EBS_REGION=$EBS_REGION \
+    EBS_ACCESSKEY=$EBS_ACCESSKEY \
+    EBS_SECRETKEY=$EBS_SECRETKEY \
+    --grant-all-permissions
+
+docker plugin install rexray/s3fs \
+    S3FS_ACCESSKEY=$S3FS_ACCESSKEY \
+    S3FS_SECRETKEY=$S3FS_SECRETKEY \
+    --grant-all-permissions
+
+docker plugin install rexray/efs \
+    EFS_ACCESSKEY=$EFS_ACCESSKEY \
+    EFS_SECRETKEY=$EFS_SECRETKEY \
+    EFS_TAG=rexray \
+    --grant-all-permissions
 `;
 };
 
 const setupNodeManagerCreationControl = (stackName, credentials) => {
   return `
 # {id:setupNodeManagerCreationControl}
-DOCKER_SWARM_JOINTOKEN_WORKER=$(docker swarm join-token worker --quiet)
-EC2_INSTANCE_LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
-DOCKER_SWARM_MANAGER_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
-
 export AWS_ACCESS_KEY_ID=${credentials.aws_access_key_id}
 export AWS_SECRET_ACCESS_KEY=${credentials.aws_secret_access_key}
 export AWS_DEFAULT_REGION=${credentials.aws_region}
+
+DOCKER_SWARM_JOINTOKEN_WORKER=$(docker swarm join-token worker --quiet)
+EC2_INSTANCE_LOCAL_IPV4=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+DOCKER_SWARM_MANAGER_INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 
 aws ssm put-parameter \
   --name "/${stackName}/swarmCluster/managers/$DOCKER_SWARM_MANAGER_INSTANCE_ID/jointoken/as/worker" \
